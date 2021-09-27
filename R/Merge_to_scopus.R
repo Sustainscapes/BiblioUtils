9bc5a9772865d926de8ff753f9f3acd0c6d571c2
derek-corcoran-barrios <derek.corcoran.barrios@gmail.com>
2021-09-27 12:53:22 +0200

    Fixed first function

--- R/Merge_to_scopus.R
+++ R/Merge_to_scopus.R
@@ -4,15 +4,20 @@
 #' @param Wos A path to a .bib file from a Web of Science search.
 #'
 #' @return A data frame with the results in Scopus format
+#' @importFrom bibliometrix convert2df
 #' @export
 #'
 #' @examples
 #'
-#' Test <- MergeToScopus(Scopus = system.file("scopus16aug.bib", package="BiblioUtils"), Wos = system.file("WOS11aug.bib", package="BiblioUtils"))
+#' Test <- MergeToScopus(Scopus = system.file("extdata/scopus16aug.bib", package="BiblioUtils"),
+#'                       Wos = system.file("extdata/WOS11aug.bib", package="BiblioUtils"))
 #'
 
 MergeToScopus <- function(Scopus, Wos){
-  SCOPUS <-bibliometrix::convert2df(file = Scopus, dbsource = "scopus", format = "bibtex")
-  WOS <-bibliometrix::convert2df(file = Wos, dbsource = "wos", format = "bibtex")
-  return(list(Scopus = SCOPUS, Wos = WOS))
+  SCOPUS <- suppressMessages(bibliometrix::convert2df(file = Scopus, dbsource = "scopus", format = "bibtex"))
+  WOS <- suppressMessages(bibliometrix::convert2df(file = Wos, dbsource = "wos", format = "bibtex"))
+  SCOPUS[setdiff(names(WOS), names(SCOPUS))] <- NA
+  WOS[setdiff(names(SCOPUS), names(WOS))] <- NA
+  DF <- rbind(SCOPUS, WOS)
+  return(DF)
 }
